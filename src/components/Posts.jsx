5e778da3c7fc54a733e5d3e660d508c148fb37c1
Danilo Leão <olin_ad@hotmail.com>
2022-11-15 11:04:44 -0300

    added red

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 function Posts() {
   const [saved, setSaved] = useState(false);
   const [liked, setLiked] = useState(false);
+  const [red, setRed] = useState(false);
   const [icon, setIcon] = useState('bookmark-outline');
   const [heart, setHeart] = useState('heart-outline');
 
@@ -58,7 +59,7 @@ function Posts() {
       <div className="fundo">
         <div className="acoes">
           <div>
-            <ion-icon name={heart} onClick={() => setLiked(!liked)}></ion-icon>
+            <ion-icon name={heart} onClick={() => setLiked(!liked)} className={red}></ion-icon>
             <ion-icon name="chatbubble-outline"></ion-icon>
             <ion-icon name="paper-plane-outline"></ion-icon>
           </div>
