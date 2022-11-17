3f3527dc0d4077a6134f4cfa4a53c3274867cdff
Danilo Leão <olin_ad@hotmail.com>
2022-11-17 10:43:03 -0300

    adjusted break lines in code

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 function Posts() {
   console.log('rendered')
@@ -38,7 +38,7 @@ function Posts() {
           meowed
         </div>
         <div className="acoes">
-          <ion-icon name="ellipsis-horizontal"></ion-icon>
+          <ion-icon name="ellipsis-horizontal" />
         </div>
       </div>
 
@@ -46,39 +46,67 @@ function Posts() {
         <img src={`assets/${image}.svg`} alt='logo' onDoubleClick={() => {
           setPostInfos((prevState) => {
             prevState[index].liked = true;
-            prevState[index].imageClicked = prevState[index].imageClicked === 'noClicked' ? 'clicked' : 'noClicked';
+            prevState[index].imageClicked = prevState[index].imageClicked === 'noClicked'
+              ? 'clicked'
+              : 'noClicked';
             return [...prevState];
           });
         }} data-test="post-image" />
-        <ion-icon name="heart" className={postInfos[index].imageClicked === 'clicked' ? 'heart-animation' : postInfos[index].imageClicked === 'noClicked' ? 'heart-animation2' : 'heart-hide'}></ion-icon>
+        <ion-icon
+          name="heart"
+          className={
+            postInfos[index].imageClicked === 'clicked'
+              ? 'heart-animation'
+              : postInfos[index].imageClicked === 'noClicked'
+                ? 'heart-animation2'
+                : 'heart-hide'}
+        />
       </div>
 
       <div className="fundo">
         <div className="acoes">
           <div>
-            <ion-icon name={postInfos[index].liked === true ? 'heart' : 'heart-outline'} onClick={() => {
-              setPostInfos((prevState) => {
-                prevState[index].liked = !prevState[index].liked;
-                return [...prevState];
-              });
-            }} data-test="like-post" className="heart-menu"></ion-icon>
-            <ion-icon name="chatbubble-outline"></ion-icon>
-            <ion-icon name="paper-plane-outline"></ion-icon>
+            <ion-icon
+              name={postInfos[index].liked === true ? 'heart' : 'heart-outline'}
+              onClick={() => {
+                setPostInfos((prevState) => {
+                  prevState[index].liked = !prevState[index].liked;
+                  return [...prevState];
+                });
+              }}
+              data-test="like-post"
+              className="heart-menu"
+            />
+            <ion-icon name="chatbubble-outline" />
+            <ion-icon name="paper-plane-outline" />
           </div>
           <div>
-            <ion-icon name={postInfos[index].saved === true ? 'bookmark' : 'bookmark-outline'} onClick={() => {
-              setPostInfos((prevState) => {
-                prevState[index].saved = !prevState[index].saved;
-                return [...prevState];
-              });
-            }} data-test="save-post"></ion-icon>
+            <ion-icon
+              name={postInfos[index].saved === true ? 'bookmark' : 'bookmark-outline'}
+              onClick={() => {
+                setPostInfos((prevState) => {
+                  prevState[index].saved = !prevState[index].saved;
+                  return [...prevState];
+                });
+              }}
+              data-test="save-post"
+            />
           </div>
         </div>
 
         <div className="curtidas">
           <img src={`assets/${likesUser}.svg`} alt='logo' />
           <div className="texto">
-            Curtido por <strong>{likesUser}</strong> e <strong>outras <span data-test="likes-number">{postInfos[index].liked === true ? formater.format(likesCounter + 1) : formater.format(likesCounter)}</span> pessoas</strong>
+            Curtido por <strong>
+              {likesUser}
+            </strong> e outras <strong>
+              <span data-test="likes-number">
+                {
+                  postInfos[index].liked === true
+                    ? formater.format(likesCounter + 1)
+                    : formater.format(likesCounter)
+                }
+              </span> pessoas </strong>
           </div>
         </div>
       </div>
