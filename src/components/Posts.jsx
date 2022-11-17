e3f34ad2354c94797018b50f5aab89002c8c3328
Danilo Leão <olin_ad@hotmail.com>
2022-11-17 19:51:17 -0300

    refatored using props passing due to project requirements

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
+import Post from './Post';
 
 function Posts() {
-  console.log('rendered')
   const postsInitialInfos = [
     {
       author: 'meowed',
@@ -17,6 +17,15 @@ function Posts() {
       image: 'dog',
       likesUser: 'adorable_animals',
       likesCounter: 99159,
+      liked: false,
+      imageClicked: '',
+      saved: false,
+    },
+    {
+      author: 'chibirdart',
+      image: 'dog',
+      likesUser: 'smallcutecats',
+      likesCounter: 85123,
       liked: false,
       imageClicked: '',
       saved: false,
@@ -51,81 +60,19 @@ function Posts() {
     const counterFormated = formater.format(likesCounter + 1);
 
     return (
-      <div
-        className="post"
+      <Post
         key={index}
-        data-test="post"
-      >
-        <div className="topo">
-          <div className="usuario">
-            <img src={`assets/${author}.svg`} alt='logo' />
-            meowed
-          </div>
-          <div className="acoes">
-            <ion-icon name="ellipsis-horizontal" />
-          </div>
-        </div>
-
-        <div className="conteudo">
-          <img
-            src={`assets/${image}.svg`}
-            alt='logo'
-            onDoubleClick={() => setPostInfos((prevState) => {
-              prevState[index].liked = true;
-              prevState[index].imageClicked = (
-                prevState[index].imageClicked === 'noClicked' ? 'clicked' : 'noClicked'
-              );
-
-              return [...prevState];
-            })}
-            data-test="post-image"
-          />
-          <ion-icon
-            name="heart"
-            className={imageHeartHandle}
-          />
-        </div>
-
-        <div className="fundo">
-          <div className="acoes">
-            <div>
-              <ion-icon
-                name={postLiked ? 'heart' : 'heart-outline'}
-                onClick={() => setPostInfos((prevState) => {
-                  prevState[index].liked = !prevState[index].liked;
-
-                  return [...prevState];
-                })}
-                data-test="like-post"
-                className="heart-menu"
-              />
-              <ion-icon name="chatbubble-outline" />
-              <ion-icon name="paper-plane-outline" />
-            </div>
-            <div>
-              <ion-icon
-                name={postSaved ? 'bookmark' : 'bookmark-outline'}
-                onClick={() => setPostInfos((prevState) => {
-                  prevState[index].saved = !prevState[index].saved;
-
-                  return [...prevState];
-                })}
-                data-test="save-post"
-              />
-            </div>
-          </div>
-
-          <div className="curtidas">
-            <img src={`assets/${likesUser}.svg`} alt='logo' />
-            <div className="texto">
-              Curtido por <strong> {likesUser} </strong> e outras <strong>
-                <span data-test="likes-number">
-                  {postLiked ? counterFormated : clickedCounterFormated}
-                </span> pessoas </strong>
-            </div>
-          </div>
-        </div>
-      </div>
+        index={index}
+        author={author}
+        imageHeartHandle={imageHeartHandle}
+        image={image}
+        setPostInfos={setPostInfos}
+        postLiked={postLiked}
+        postSaved={postSaved}
+        likesUser={likesUser}
+        counterFormated={counterFormated}
+        clickedCounterFormated={clickedCounterFormated}
+      />
     )
   });
 
